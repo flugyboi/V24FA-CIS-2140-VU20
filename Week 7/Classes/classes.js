ea107674b3664557f7e2209ed7cdfdc71224c079
Robert Johnson <rbrucej@gmail.com>
2024-10-18 01:27:13 -0400

    Add Week 7 JS files

---
+++ Week 7/Classes/classes.js
@@ -0,0 +1,15 @@
+
+/*
+ * This file makes an object class for blog posts that will have different display methods for different types of posts.
+ * Assignment: Add a third type of blog post: AnnouncementPost. Have this be text inside an h1 tag so the text is very big. Add a section to posts.js that uses this new type.
+ */
+
+class BlogPost {
+
+}
+
+var display = document.getElementById("display");
+posts.forEach(postDefinition => {
+    // Display the posts...
+});
+
---
+++ Week 7/Cookies/cookies.js
@@ -0,0 +1,9 @@
+/*
+ * This script will make a cookie that remembers your visits for the age of the session.
+ * Assignment: Change the cookie to be a visit counter. Have it add 1 every time you visit the page and display that number to you.
+ */
+var cookieName = "cis-2140-text-cookie";
+
+function setCookie(event)
+{
+}
