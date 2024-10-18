863688c5b6f46131144a6a5af0edb9fb866afb72
Robert Johnson <rbrucej@gmail.com>
2024-10-18 12:07:41 -0400

    Classes exercise

--- Week 7/Classes/classes.js
+++ Week 7/Classes/classes.js
@@ -5,11 +5,62 @@
  */
 
 class BlogPost {
+    title;
+    content;
 
+    constructor(definition)
+    {
+        this.title = definition.title;
+        this.content = definition.content;
+    }
+
+    drawContent(element)
+    {
+        var titleTag = document.createElement("div");
+        titleTag.innerText = this.title;
+        element.appendChild(titleTag);
+
+        var contentTag = document.createElement("div");
+        contentTag.innerText = this.content;
+        element.appendChild(contentTag);
+
+        element.appendChild(document.createElement("hr"));
+    }
+
+    static createPost(definition)
+    {
+        if (definition.type === "text")
+        {
+            return new BlogPost(definition);
+        }
+        else if (definition.type === "image")
+        {
+            return new ImagePost(definition);
+        }
+    }
+}
+
+class ImagePost extends BlogPost
+{
+    drawContent(element)
+    {
+        var titleTag = document.createElement("div");
+        titleTag.innerText = this.title;
+        element.appendChild(titleTag);
+
+        var contentTag = document.createElement("img");
+        contentTag.src = this.content;
+        contentTag.width = 500;
+        element.appendChild(contentTag);
+
+        element.appendChild(document.createElement("hr"));
+    }
 }
 
 var display = document.getElementById("display");
 posts.forEach(postDefinition => {
-    // Display the posts...
+    var post = BlogPost.createPost(postDefinition);
+    console.log(post.title);
+    post.drawContent(display);
 });
 
